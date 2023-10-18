0879c512d2f18e87657dad12f0d4e07cb6f2dcde
arenm <areum59@soleo.kr>
2023-10-19 08:37:09 +0900

    uuid 추가
    ID의 고유한 값을 랜덤으로 생성해주는 라이브러리 사용

--- todo/src/components/AddTodo/AddTodo.jsx
+++ todo/src/components/AddTodo/AddTodo.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function AddTodo({onAdd}) {
     const [text, setText] = useState();
-    const handleChange = (e) => setText(e.target.value);
+    // const handleChange = (e) => setText(e.target.value);
     const onSubmit = (e) => {
         e.preventDefault();  // 화면이 리프레쉬 되는 것을 방지
         if (text.trim().length === 0) {
             return;
         }  // text의 앞, 뒤 공백 제거 후 length가 0이라면 값을 반환하지 않고 넘어간다.
-        onAdd({id: '고유한값', text, status: 'active'});
+        onAdd({id: uuidv4(), text, status: 'active'});
         setText('');  // 텍스트를 추가한 후 input 초기화
     }
 
     return (
       <form onSubmit={onSubmit}>
-        <input
+        <input 
           type="text"
           placeholder="할 일을 추가하세요"
           value={text}
-          onChange={handleChange}
+          onChange={e => setText(e.target.value)}
         />
         <button>추가</button>
       </form>
