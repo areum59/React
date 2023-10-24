8f74a6525a008337eb83a30e8495d67f3bdd54ed
arenm <areum59@soleo.kr>
2023-10-24 10:27:32 +0900

    필터 적용하기

--- todo/src/components/TodoList/TodoList.jsx
+++ todo/src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({filter}) {
     // Todos의 기본 값
     const [todos, setTodos] = useState([
         {id: '123', text: '장보기', status: 'active'},
@@ -17,10 +17,12 @@ export default function TodoList() {
     const handleDelete = (deleted) => 
         setTodos(todos.filter((t) => t.id !== deleted.id));
 
+    const filtered = getFilteredItems(todos, filter);
+
     return (
       <section>
         <ul>
-            {todos.map(item => (
+            {filtered.map(item => (
                 <Todo
                     key={item.id}
                     todo={item}
@@ -34,4 +36,11 @@ export default function TodoList() {
         {/* Callback함수를 통하여 onAdd={}를 props로 전달, handleAdd를 호출 */}
       </section>
     );
+
+    function getFilteredItems(todos, filter){
+        if (filter === 'all') {
+            return todos
+        }
+        return todos.filter((todo) => todo.status === filter);
+    }
 }
