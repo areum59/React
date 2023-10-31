d69d751775923ac45eade9d34605c8ec62b99261
arenm <areum59@soleo.kr>
2023-10-31 15:29:47 +0900

    다크모드 구현

--- todo/src/components/Header/Header.jsx
+++ todo/src/components/Header/Header.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import { useDarkMode } from '../../context/DarkModeContext';
 import styles from './Header.module.css'
+import { HiMoon, HiSun } from 'react-icons/hi'
 
 export default function Header({filters, filter, onFilterChange}) {
-    return (
-      <header className={styles.header}>
-        <ul className={styles.filters}>
-          {filters.map((value, index) => (
-            <li key={index}>
-              <button
-                className={`${styles.filter} ${
-                    filter === value && styles.selected
-                    // 전달된 filter의 값이 버튼의 값이 같다면 'selected' className 추가
-                }`}
-                onClick={() => onFilterChange(value)}
-                >
-                {value}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </header>
-    );
+  const {darkMode, toggleDarkMode} = useDarkMode();
+  
+  return (
+    <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
+
+      <ul className={styles.filters}>
+        {filters.map((value, index) => (
+          <li key={index}>
+            <button
+              className={`${styles.filter} ${
+                  filter === value && styles.selected
+                  // 전달된 filter의 값이 버튼의 값이 같다면 'selected' className 추가
+              }`}
+              onClick={() => onFilterChange(value)}
+              >
+              {value}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 }
