81494866ea2f062ecd187277ed78d1dc83079081
arenm <areum59@soleo.kr>
2023-11-14 17:37:17 +0900

    Header 스타일링
    tailwindcss 추가

--- youtube/src/components/SearchHeader.jsx
+++ youtube/src/components/SearchHeader.jsx
@@ -18,21 +18,24 @@ export default function SearchHeader() {
     // 검색창 내용이 리디렉션 후에도 히스토리에 따라 동시에 변동될 수 있도록 함.
 
     return (
-      <header>
-        <Link to='/'>
-            <BsYoutube />
-            <h1>MyTube</h1>
+      <header className='w-full flex p-4 text-2xl border-b border-zinc-600 mb-4'>
+        <Link to='/' className='flex items-center'>
+            <BsYoutube className='text-4xl text-brand mr-1' />
+            <h1 className='text-2x font-semibold'>MyTube</h1>
         </Link>
 
-        <form onSubmit={handleSubmit}>
-            <div>
+        <form className='w-full flex justify-center' onSubmit={handleSubmit}>
+            <div className='w-5/12 flex items-center rounded-full overflow-hidden border-solid border-1 border-zinc-600'>
                 <input
+                    className='flex-1 h-full text-sm py-2 px-6 outline-none bg-black text-gray-50'
                     type="text"
-                    placeholder="검색"
+                    placeholder="Search..."
                     value={text}
                     onChange={e => setText(e.target.value)}
                 />
-                <button><FiSearch /></button>
+                <button className='h-full border-l border-zinc-600 bg-zink-600 px-8'>
+                    <FiSearch />
+                </button>
             </div>
         </form>
       </header>
