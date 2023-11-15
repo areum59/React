41e14b7ebcc137ce13d4ab50f33feded0a3b7fae
arenm <areum59@soleo.kr>
2023-11-15 15:19:52 +0900

    VideoCard - axios 라이브러리 추가
    fetch의 문제점을 보완하기위해
    안전성 있는 라이브러리로 대체.

--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query'
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 
 export default function Videos() {
     const { keyword } = useParams();
-    const { isLoding, error, data: videos } = useQuery({
-        queryKey: ['videos', keyword],  // 배열의 형태고 지정.
+    const {
+        isLoding,
+        error,
+        data: videos,
+    } = useQuery({
+        queryKey: ["videos", keyword], // 배열의 형태로 지정.
         queryFn: async () => {
-            return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-                .then((res) => res.json())
-                .then((data) => data.items);
-        }
-    })
+            return axios
+            .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+            .then((res) => res.data.items);
+        },
+    });
 
     return (
       <>
