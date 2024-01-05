947476609fe842e8b9823aa5c0cb50c92afbefdc
areum59 <reddoll8859@gmail.com>
2024-01-05 09:43:58 +0900

    ant design 프로젝트 추가

---
+++ ant-design/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormMethods from './components/FormMethods';
+
+function App() {
+  return (
+    <div className="App">
+      <FormMethods />
+    </div>
+  );
+}
+
+export default App;
